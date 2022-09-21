e62ff4fcb29b6da95fb3ae0e5c398a6ba7eeacfc
BigG <alphafleetacc@gmail.com>
2022-09-21 13:18:36 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+
+const path = require("path")
+
+const PORT = process.env.PORT || 5000
+
+
+app.use(express.static(__dirname + '/'))
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/views/main.html"))
+})
+
+
+app.listen(PORT, console.log(`http://localhost:${ PORT }`))
